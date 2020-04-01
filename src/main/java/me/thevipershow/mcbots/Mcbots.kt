0efeb492a48237dae1c1cf8228d40b93468ab095
fuomag9 <fuomag@gmail.com>
2020-04-01 23:56:51 +0200

    Converted main class to kotlin

---
+++ src/main/java/me/thevipershow/mcbots/Mcbots.kt
@@ -0,0 +1,14 @@
+package me.thevipershow.mcbots
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Mcbots : JavaPlugin() {
+    override fun onEnable() {
+
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
