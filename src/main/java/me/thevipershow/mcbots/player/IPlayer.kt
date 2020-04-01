43ae2d3ba37b7f0f7a342593fd4320fb7bb90245
fuomag9 <fuomag@gmail.com>
2020-04-02 00:00:50 +0200

    Update IPlayer.kt

--- src/main/java/me/thevipershow/mcbots/player/IPlayer.kt
+++ src/main/java/me/thevipershow/mcbots/player/IPlayer.kt
@@ -1,4 +1,16 @@
 package me.thevipershow.mcbots.player
 
 interface IPlayer {
+    var name : String
+
+    fun place_block() : Boolean
+    fun destroy_block() : Boolean
+    fun run() : Boolean
+    fun crawl() : Boolean
+    fun uncrawl() : Boolean
+    fun rotate() : Boolean
+    fun walk() : Boolean
+    fun attack() : Boolean
+    fun jump() : Boolean
+    fun write_to_chat(message : String) : Boolean
 }
